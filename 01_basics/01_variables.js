79796e26771d8430cc1f2142dfaedaec9f878cf0
Anshikashri456 <144266751+Anshikashri456@users.noreply.github.com>
2024-12-01 09:58:36 +0000

    data_Type

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "anshikashrivastava0154@gmai.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+//accountID = "2"//
+
+console.log(accountId);
+accountEmail = "hchc@gamil.com"
+accountPassword = "21212121217"
+accountCity = "bengulauru"
+
+ 
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; //treat all js code as newer version.
+
+/*alert()
+alert("hello")
+alert(3+3)we are using node js not browser */
+
+
+console.log(3+3)
+console.log("Anshika")
+
+let name ="hitesh"
+let age = 18
+let isLoggedIn = false/true
+let state;
+
+//number => 2 to pwer 53
+//bigint
+//string =>""
+//boolean =>true/false
+//null =. standalone value
+//undefined =>
+//symbol =>unique
+
+//object
+
+
+console.log(typeof "hitesh")//string
+console.log(typeof age);//number
+console .log(typeof null)//object
+console.log(typeof undefined)//undefined
